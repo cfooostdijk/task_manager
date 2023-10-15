181392e7186a64a92f80d625d78717c96e292300
CrisOos <cristian.oostdijk@avenida.com>
2023-10-15 11:15:55 -0300

    Controllers methods, views and routes for Tasks and Main

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,14 @@
 class MainController < ApplicationController
+
   def index
   end
 
   def about
+    @created_by = "Cristian"
   end
+
+  def hello
+    redirect_to(action: 'index')
+  end
+
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
+
   def index
+    @tasks = ['Fold laundry', 'Sweep porch', 'Wash dishes', 'Mow lawn']
   end
 
   def new
