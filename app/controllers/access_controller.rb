4fb60db17dec3a52e264e38598d30a2a1270ddcc
CrisOos <cristian.oostdijk@avenida.com>
2023-10-15 23:12:36 -0300

    Avoid double rendered, different renders, shared layouts and content_for

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -2,9 +2,30 @@ class AccessController < ApplicationController
   skip_before_action :confirm_logged_in, only: [:new, :create]
   # before_action :get_user_info_from_session, only: [:menu, :new] PASO AL APPLICATION CONTROLLER ASI LO PUEDO USAR EN OTROS CONTROLLERS
 
-
   def menu
     # get_user_info_from_session LO SACO YA QUE LO TENGO EN EL BEFORE ACTION
+
+    # Examples of render options:
+    #
+    # render('menu')
+    #
+    # render(plain: "Just render text.")
+    #
+    # render(html: "<strong>HTML text</strong>")
+    #
+    # render(json: Task.first)
+    #
+    # render(xml: ['dog', 'cat', 'mouse'])
+    #
+    # render(plain: 'OK', status: 200)
+    #
+    # path_to_404 = Rails.root.join('public', '404.html')
+    # render(file: path_to_404)
+    #
+    # send_file(path_to_404)
+    #
+    # string = render_to_string(file: path_to_404)
+    # logger.debug(string)
   end
 
   def new
@@ -27,7 +48,7 @@ class AccessController < ApplicationController
   def destroy
     logger.info("*** Logout User #{cookies[:username]}")
 
-    cookies[:username] = params[:username]
+    cookies[:username] = nil
     session[:user_id] = nil
 
     flash[:notice] = 'Logged out'
